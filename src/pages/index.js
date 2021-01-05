2e797500bf615ce7397a4fc742ae69f8a5961c0f
sylviaxliu <sylviaxliu@yahoo.com>
2021-01-05 18:27:46 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>welcome to {data.site.siteMetadata.title}</h1>
+      <p>hello this will eventually be my website</p>
+    </Layout>
+    )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`

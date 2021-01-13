b9251a610a397e799e9d1faed343ac3080b01250
sylviaxliu <sylviaxliu@yahoo.com>
2021-01-12 23:26:43 -0500

    Create extracurricular component

---
+++ src/components/Extracurricular.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../styles/extracurricular.scss"
+
+export default function Extracurricular ( props ) {
+    return (
+        <div id="extracurricular">
+            { props.name }
+        </div>
+    )
+}

ccfa794e077c92fbb66bc1c9755ee0cac9288d3c
sylviaxliu <sylviaxliu@yahoo.com>
2021-01-12 23:41:27 -0500

    update main

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,16 +18,16 @@ export default function About() {
 
           <h2 style={{ textAlign: `right` }}>Experience</h2>
           <p>
-            <Experience name="Name of Job" 
-              startDate="start date of job" 
-              endDate="end date of job" 
-              role="this is my role" 
+            <Experience name="Undergraduate Student Researcher" 
+              startDate="Aug. 2020" 
+              endDate="May 2021" 
+              company="Lawerence Livermore National Laboratory, Purdue University Collaboration" 
               description="this is what i did, probably in somewhat complete sentences." 
             />
-            <Experience name="name of other job" 
-              startDate="start date of job" 
-              endDate="end date of job" 
-              role="this is my role" 
+            <Experience name="Data Management Intern" 
+              startDate="Dec. 2020" 
+              endDate="Jan. 2021" 
+              company="alpha Kappa Delta Phi International Leadership Board" 
               description="this is what i did in another sentence." 
             />
           </p>
@@ -36,42 +36,28 @@ export default function About() {
           <p>
             <Extracurricular name="alpha Kappa Delta Phi International Sorority, Inc." />
               <Role name="Treasurer"
-                startDate="beginning"
-                endDate="ending"
                 description="probably bullet points right"
               />
               <Role name="Secretary"
-                startDate="beginning"
-                endDate="ending"
                 description="this is what i did in this role"
               />
               <Role name="Fundraising Chair"
-                startDate="beginning"
-                endDate="ending"
                 description="this is what i did in this role"
               />
             <Extracurricular name="Purdue Asian Student Union Board" />
               <Role name="Fundraising Chair"
-                startDate="beginning"
-                endDate="ending"
                 description="probably bullet points right"
               />
             <Extracurricular name="Purdue Multicultural Greek Council" />
               <Role name="Treasurer"
-                startDate="beginning"
-                endDate="ending"
                 description="probably bullet points right"
               />
             <Extracurricular name="Society for Asian Scientists and Engineers" />
               <Role name="General Member"
-                startDate="beginning"
-                endDate="ending"
                 description="probably bullet points right"
               />
             <Extracurricular name="Asian American Association" />
               <Role name="General Member"
-                startDate="beginning"
-                endDate="ending"
                 description="probably bullet points right"
               />
           </p>

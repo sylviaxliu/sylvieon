0b9c61b33a83f45d83d2c4b18717da5794015286
sylviaxliu <sylviaxliu@yahoo.com>
2021-01-12 23:27:22 -0500

    Update main styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Experience from "../components/experience"
+import Extracurricular from "../components/extracurricular"
 import Header from "../components/header"
 import Layout from "../components/Layout"
+import Role from "../components/Role"
 import { graphql, Link } from "gatsby"
 
 export default function About() {
@@ -14,7 +16,7 @@ export default function About() {
 
           <p>hello!! I'm Sylvia, a sophomore majoring in Computer Science at Purdue University. i'm doing a concentration in machine learning and security but making websites is really fun. i spend too much money on coffee.</p>
 
-          <h2 style={{ textAlign: `right` }}>experience</h2>
+          <h2 style={{ textAlign: `right` }}>Experience</h2>
           <p>
             <Experience name="Name of Job" 
               startDate="start date of job" 
@@ -30,8 +32,49 @@ export default function About() {
             />
           </p>
           
-          <h2 style={{ textAlign: `left` }}>extra curriculars</h2>
-          <p>akdphi, sase, mgc, asub</p>
+          <h2 style={{ textAlign: `left` }}>Extracurriculars</h2>
+          <p>
+            <Extracurricular name="alpha Kappa Delta Phi International Sorority, Inc." />
+              <Role name="Treasurer"
+                startDate="beginning"
+                endDate="ending"
+                description="probably bullet points right"
+              />
+              <Role name="Secretary"
+                startDate="beginning"
+                endDate="ending"
+                description="this is what i did in this role"
+              />
+              <Role name="Fundraising Chair"
+                startDate="beginning"
+                endDate="ending"
+                description="this is what i did in this role"
+              />
+            <Extracurricular name="Purdue Asian Student Union Board" />
+              <Role name="Fundraising Chair"
+                startDate="beginning"
+                endDate="ending"
+                description="probably bullet points right"
+              />
+            <Extracurricular name="Purdue Multicultural Greek Council" />
+              <Role name="Treasurer"
+                startDate="beginning"
+                endDate="ending"
+                description="probably bullet points right"
+              />
+            <Extracurricular name="Society for Asian Scientists and Engineers" />
+              <Role name="General Member"
+                startDate="beginning"
+                endDate="ending"
+                description="probably bullet points right"
+              />
+            <Extracurricular name="Asian American Association" />
+              <Role name="General Member"
+                startDate="beginning"
+                endDate="ending"
+                description="probably bullet points right"
+              />
+          </p>
           
         </div>
       </Layout>
